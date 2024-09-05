2b23618d63c4b3d850f0675249ca779764735531
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-09-04 21:06:43 -0300

    commit inicial

---
+++ src/test/java/com/barbosa/email/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa.email.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
